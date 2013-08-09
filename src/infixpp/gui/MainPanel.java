e25a69b67db0419f987837a4564240a8a32acc17
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2013-08-10 03:26:51 +0900

    implement image output

--- src/infixpp/gui/MainPanel.java
+++ src/infixpp/gui/MainPanel.java
@@ -5,7 +5,12 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.Transparency;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
@@ -50,22 +55,33 @@ public class MainPanel extends JPanel
 		layoutCalculator.setVerticalGap(vgap);
 	}
 
-	private void draw(Graphics2D g)
+	public void saveImage(File file) throws IOException
+	{
+		layoutCalculator.layout(treeGraph);
+		int width = layoutCalculator.getWidth() + 40;
+		int height = layoutCalculator.getHeight() + 40;
+		BufferedImage image = new BufferedImage(width, height, Transparency.OPAQUE);
+		Graphics2D g = (Graphics2D)image.getGraphics();
+		draw(g, width, height);
+		g.dispose();
+		ImageIO.write(image, "png", file);
+	}
+
+	private void draw(Graphics2D g, int width, int height)
 	{
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
 		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, getWidth(), getHeight());
+		g.fillRect(0, 0, width, height);
 
-		layoutCalculator.layout(treeGraph);
-
-		int x0 = (getWidth() - layoutCalculator.getWidth()) / 2;
-		int y0 = (getHeight() - layoutCalculator.getHeight()) / 2;
+		int x0 = (width - layoutCalculator.getWidth()) / 2;
+		int y0 = (height - layoutCalculator.getHeight()) / 2;
 		drawer.draw(g, x0, y0, layoutCalculator.getNodeSize(), treeGraph);
 	}
 
 	protected void paintComponent(Graphics g)
 	{
-		draw((Graphics2D)g);
+		layoutCalculator.layout(treeGraph);
+		draw((Graphics2D)g, getWidth(), getHeight());
 	}
 }
